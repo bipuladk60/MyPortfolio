0226b4fda976292b21c1c07ead58e33b5cdb6b27
bipuladk60 <bipuladk60@gmail.com>
2025-03-06 09:57:15 -0600

    fixed the color bug

--- src/backend/CmntSubmit.js
+++ src/backend/CmntSubmit.js
@@ -1,27 +1,27 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const handleSubmit = async (event, name, email, message, setResponse, setName, setEmail, setMessage) => {
-    event.preventDefault();
+// export const handleSubmit = async (event, name, email, message, setResponse, setName, setEmail, setMessage) => {
+//     event.preventDefault();
 
-    const { data, error } = await supabase
-        .from('comment')
-        .insert([{ name, email, comment: message }])
-        .select()
+//     const { data, error } = await supabase
+//         .from('comment')
+//         .insert([{ name, email, comment: message }])
+//         .select()
 
-        if (error) {
-            setResponse('Error submitting comment');
-            console.log('error')
-        }
-        else {
-            setName('');
-            setEmail('');
-            setMessage('');
-            setResponse('I am still working on this feature. Your comment will be saved for later.');
-        }
+//         if (error) {
+//             setResponse('Error submitting comment');
+//             console.log('error')
+//         }
+//         else {
+//             setName('');
+//             setEmail('');
+//             setMessage('');
+//             setResponse('I am still working on this feature. Your comment will be saved for later.');
+//         }
 
-}
+// }
 
